2b68a009cc31c175a421d22752dcf9d094e58dbf
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2020-06-13 15:18:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 welcome to my webserver
-<br />
-<pre>
-<?php
-<img src = '/var/www/html/skillset.jpg' width ='200' height ='200' />
+<img src = 'https://bucket-for-webserver.s3.ap-south-1.amazonaws.com/first-img.jpeg' width ='200' height ='200' />
 print 'ifconfig';
-?/
-</pre>
