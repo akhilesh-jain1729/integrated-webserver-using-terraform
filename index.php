e65e343696b24f069237169b80b4a22b5d8979d8
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2021-03-21 13:40:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-welcome to my webserver
+This is from S3 Bucket URL
 <img src = 'https://bucket-for-webserver.s3.ap-south-1.amazonaws.com/first-img.jpeg' width ='200' height ='200' />
+
+This is from CloudFront URL
+<img src = 'https://bucket-for-webserver.s3.ap-south-1.amazonaws.com/first-img.jpeg' width ='200' height ='200' />
+
 
 # instead of S3 object url - give Cloudfront domain 
